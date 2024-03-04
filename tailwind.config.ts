61aeb75e1c6e05cf6e58c8994e5cd8a487fab6ec
Ethereumistic <badjarovv@gmail.com>
2024-03-04 13:49:25 +0200

    fixing issues 2

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -38,11 +38,11 @@ const config: Config = {
       keyframes: {
         spotlight: {
           "0%": {
-            opacity: 0,
+            opacity: "0",
             transform: "translate(-72%, -62%) scale(0.5)",
           },
           "100%": {
-            opacity: 1,
+            opacity: "1",
             transform: "translate(-61%,-30%) scale(1)",
           },
         },
